28fbd4823b8fa91bf8bb48faf53e25a0031f95db
jeager000 <58515902+jeager000@users.noreply.github.com>
2020-01-15 18:27:56 +0700

    Update send.py

--- send.py
+++ send.py
@@ -1,7 +1,7 @@
 import yagmail
 
-x=yagmail.SMTP('cacacrackphiss@gmail.com','jeager1999$')
+x=yagmail.SMTP('email_sender@gmail.com','password')
 subject='example'
 body=str(input('Your message: '))
-x.send('cacacrack000@gmail.com',subject,body)
+x.send('email_receiver@gmail.com',subject,body)
 print('Success')
